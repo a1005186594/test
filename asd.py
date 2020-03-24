029e16106aa9cb099003163c1f692d57d0704285
a1005186594 <1005186594@qq.com>
2020-03-24 20:06:24 +0800

    test2

--- asd.py
+++ asd.py
@@ -9,6 +9,7 @@ import random
 import keras as K
 from keras.layers import Conv2D, MaxPooling2D,Dense, Dropout, Flatten
 
+# dasdasdsad
 # 验证码中的字符, 不用汉字
 number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
